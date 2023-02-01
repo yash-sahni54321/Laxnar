14812c487c19546425658f475cf54128e08e2dc6
Yash Sahni <67969545+yash-sahni54321@users.noreply.github.com>
2023-02-01 22:30:19 +0530

    fifth Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-
-import NavBar from './Components/NavBar';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from './Components/Navbar';
+import HomePage from './Pages/HomePage'
+import ProductPage from './Pages/ProductPage'
+import Error404Page from './Pages/Error404Page'
+import React from 'react';
 function App() {
   return (
     <div className="App">
-      Yash Sahani new 
-      rahul ranjan singh
-      Aman Sharma
+      <Navbar/>
     </div>
+  //   <Router>
+  //   <div className="App">
+  //     <NavBar />
+  //     <div className="content">
+  //       <Switch>
+  //         <Route exact path="/">
+  //           <HomePage />
+  //         </Route>
+  //         <Route path="/product">
+  //           <ProductPage/>
+  //         </Route>
+  //         <Route path="*">
+  //           <Error404Page/>
+  //         </Route>
+  //       </Switch>
+  //     </div>
+  //   </div>
+  // </Router>
+
   );
 }
 
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,5 +1,5 @@
 const HomePage = () => {
-    return (  <></>);
+    return (  <> Home </>);
 }
  
 export default HomePage;
