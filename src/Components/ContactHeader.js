538fcee9b26c0e5774440d80743c8b100cafc478
Yash Sahni <67969545+yash-sahni54321@users.noreply.github.com>
2023-02-08 02:30:18 +0530

    Carousal and about added

--- src/Components/ContactHeader.js
+++ src/Components/ContactHeader.js
@@ -81,7 +81,7 @@ const ContactHeader = () => {
                 </div>
                 <div className="p-2 w-full">
                   <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                    Button
+                    Submit
                   </button>
                 </div>
               </div>
